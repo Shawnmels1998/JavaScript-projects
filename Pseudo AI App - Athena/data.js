ff5c2fe2ad76430c99b64d46b0916be0d04be346
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-04-07 19:43:59 +0300

    Athena AI app

---
+++ Pseudo AI App - Athena/data.js
@@ -0,0 +1,19 @@
+let lists = [
+  {
+    question: "what is your name",
+    answer: "my name is Athena.",
+  },
+  {
+    question: "what's your name",
+    answer: "my name is Athena.",
+  },
+  {
+    question: "what do you do",
+    answer: "I help developers write better code.",
+  },
+  {
+    question: "introduce yourself",
+    answer:
+      "my name is Athena named after the greek goddess Athena, I am a Java Script program created by Shawn and the next Evolved CHATGPT hehehe.",
+  },
+];
