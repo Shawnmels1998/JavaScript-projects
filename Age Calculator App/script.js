183284c0605d75a20ac098185d101c7f4066c051
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-04-21 22:38:36 +0300

    updated js file

--- Age Calculator App/script.js
+++ Age Calculator App/script.js
@@ -1,63 +1,42 @@
-const username = document.querySelector("#username");
-const dob = document.querySelector("#dob");
-const btn = document.querySelector(".btn");
-const showName = document.querySelector(".u-name");
-const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// Use destructuring assignment to simplify getting references to DOM elements
+const [username, dob, btn, showName] = [  "#username",  "#dob",  ".btn",  ".u-name",].map((selector) => document.querySelector(selector));
 
-btn.addEventListener("click", calculateAge);
+// Define the array of days per month using a const instead of let
+const DAYS_PER_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-function calculateAge(e) {
+// Add an event listener to the button element with an arrow function instead of a named function
+btn.addEventListener("click", (e) => {
   e.preventDefault();
-  let today = new Date();
-  let dobInput = new Date(dob.value);
-  let birthMonth, birthDate, birthYear;
-  let birthDetails = {
-    date: dobInput.getDate(),
-    month: dobInput.getMonth() + 1,
-    year: dobInput.getFullYear(),
-  };
-  let currentYear = today.getFullYear();
-  let currentMonth = today.getMonth() + 1;
-  let currentDate = today.getDate();
 
-  if (
-    birthDetails.year > currentYear ||
-    (birthDetails.month > currentMonth && birthDetails.year == currentYear) ||
-    (birthDetails.date > currentDate &&
-      birthDetails.month == currentMonth &&
-      birthDetails.year == currentYear)
-  ) {
+  // Get the current date and the user's inputted date of birth
+  const today = new Date();
+  const dobInput = new Date(dob.value);
+
+  // Validate the input date and return early if it is invalid
+  const isInvalidDate =
+    dobInput.getFullYear() > today.getFullYear() ||
+    (dobInput.getMonth() >= today.getMonth() &&
+      dobInput.getFullYear() === today.getFullYear() &&
+      dobInput.getDate() > today.getDate());
+  if (isInvalidDate) {
     alert("Invalid date");
     return;
   }
 
-  birthYear = currentYear - birthDetails.year;
+  // Calculate the user's age in years, months, and days
+  let ageInYears = today.getFullYear() - dobInput.getFullYear();
+  let ageInMonths =
+    today.getMonth() -
+    dobInput.getMonth() +
+    12 * (ageInYears - (today.getDate() < dobInput.getDate() ? 1 : 0));
+  let ageInDays =
+    today.getDate() -
+    dobInput.getDate() +
+    (ageInMonths % 12 === 1 ? DAYS_PER_MONTH[ageInMonths - 2] : 0);
 
-  if (currentMonth >= birthDetails.month) {
-    birthMonth = currentMonth - birthDetails.month;
-  } else {
-    birthYear--;
-    birthMonth = 12 + currentMonth - birthDetails.month;
-  }
-
-  if (currentDate >= birthDetails.date) {
-    birthDate = currentDate - birthDetails.date;
-  } else {
-    birthMonth--;
-    let days = months[currentMonth - 2];
-    birthDate = days + currentDate - birthDetails.date;
-
-    if (birthMonth < 0) {
-      birthMonth = 11;
-      birthYear--;
-    }
-  }
-  displayResult(birthDate, birthMonth, birthYear);
-
-  function displayResult(bDate, bMonth, bYear) {
-    showName.textContent = username.value;
-    document.querySelector(".age-year").textContent = bYear + "Years";
-    document.querySelector(".age-month").textContent = bMonth + "Months";
-    document.querySelector(".age-day").textContent = bDate + "Day";
-  }
-}
+  // Display the calculated age in the DOM
+  showName.textContent = username.value;
+  document.querySelector(".age-year").textContent = ageInYears + "Years";
+  document.querySelector(".age-month").textContent = ageInMonths + "Months";
+  document.querySelector(".age-day").textContent = ageInDays + "Day";
+});
