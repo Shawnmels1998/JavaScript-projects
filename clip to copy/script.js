79bbf70f2b3d3a4a547d77ca3bb8e0b2ce94c501
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-04-24 21:01:25 +0300

    updated js file

--- clip to copy/script.js
+++ clip to copy/script.js
@@ -1,19 +1,23 @@
-const btn = document.querySelector(".btn");
-const coupon = document.querySelector(".coupon");
+const copyCoupon = () => {
+  const btn = document.querySelector(".btn");
+  const coupon = document.querySelector(".coupon");
 
-const copyText = (e) => {
-   e.preventDefault();
+  const copyText = (e) => {
+    e.preventDefault();
 
-   coupon.select();
-   coupon.setSelectionRange(0, 999999);
-   document.execCommand = "copy";
+    coupon.select();
+    coupon.setSelectionRange(0, 999999);
+    document.execCommand("copy");
 
-   btn.textContent = "copied!!!";
+    btn.textContent = "copied!!!";
 
-   setTimeout(() => {
-    btn.textContent = "copy";
-   },3000);
+    setTimeout(() => {
+      btn.textContent = "copy";
+    }, 3000);
+  };
+
+  btn.addEventListener("click", copyText);
 };
 
-btn.addEventListener("click", copyText);
+copyCoupon();
 
