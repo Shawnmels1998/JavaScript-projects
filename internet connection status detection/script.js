dfcd4530554f78ef31ebef53e766ff8f97a614c9
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-04-26 21:02:11 +0300

    updated js file

--- internet connection status detection/script.js
+++ internet connection status detection/script.js
@@ -1,40 +1,37 @@
 const image = document.getElementById("image");
 const statusDisplay = document.getElementById("status");
-const bgColor = document.getElementById("main");
+const main = document.getElementById("main");
 
 function setColor() {
-    bgColor.classList.add("online");
+  main.classList.add("online");
 }
 
-async function connectionStatus() {
+async function checkConnection() {
   try {
-    const fetchResult = await fetch('https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/440px-Lenna_%28test_image%29.png?time=' + (new Date()).getTime());
+    const response = await fetch('https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/440px-Lenna_%28test_image%29.png?time=' + (new Date()).getTime());
     image.src = "./images/online.jpg";
     setColor();
-    return fetchResult.status >= 200 && fetchResult.status < 300;
+    return response.status >= 200 && response.status < 300;
   } catch (error) {
     console.error(error);
     statusDisplay.textContent = "OH NO😲😲!!! Looks like your Internet Connection is Down.";
     image.src = "./images/offline.png";
-    bgColor.classList.remove("online");
+    main.classList.remove("online");
+    return false;
   }
 }
 
-
 // Monitor the connection
-  setInterval(async () => {
-    const result = await connectionStatus();
-    if ( result ) {
-        statusDisplay.textContent = "Yay 🎊🎊🎊 you are now ONLINE";
-        setColor(); 
-    } 
-  }, 5000);
-  
-//   Check Connection When Page Loads
-   window.addEventListener("load", async (event) => {
-     if ( connectionStatus() ) {
-    	statusDisplay.textContent = "Online"
-    } else {
-    	statusDisplay.textContent = "OFFline"
-    }
-  });
+setInterval(async () => {
+  const result = await checkConnection();
+  if (result) {
+    statusDisplay.textContent = "Yay 🎊🎊🎊 you are now ONLINE";
+    setColor(); 
+  } 
+}, 5000);
+
+// Check connection when page loads
+window.addEventListener("load", async () => {
+  const result = await checkConnection();
+  statusDisplay.textContent = result ? "Online" : "Offline";
+});
