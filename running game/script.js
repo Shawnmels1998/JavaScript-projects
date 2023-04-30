62d2ebab630b3dce3baa30c68d6c27ba07f848dd
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-04-30 10:16:12 +0300

    updated JS FILE

--- running game/script.js
+++ running game/script.js
@@ -3,28 +3,29 @@ const heroBoy = document.querySelector(".hero-boy");
 const villain = document.querySelector(".villain");
 
 const jump = () => {
-    if (hero.classList != "animate") {
-        hero.classList.add("animate");
-        villain.style.animation = "move 1s infinite linear";
-    }
+  if (!hero.classList.contains("animate")) {
+    hero.classList.add("animate");
+    villain.style.animation = "move 1s infinite linear";
+  }
 
-    setTimeout(() => {
-        hero.classList.remove("animate");
-    }, 300);
-}
+  setTimeout(() => {
+    hero.classList.remove("animate");
+  }, 300);
+};
 
 document.addEventListener("keydown", (e) => {
-    if (e.code == "Space") {
-        jump();
-    }
+  if (e.code === "Space") {
+    jump();
+  }
 });
 
-let isAlive = setInterval(() => {
-    let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
-    let villainLeft = parseInt(window.getComputedStyle(villain).getPropertyValue("left"));
+const isAlive = setInterval(() => {
+  const heroTop = parseInt(window.getComputedStyle(hero).top);
+  const villainLeft = parseInt(window.getComputedStyle(villain).left);
 
-    if (villainLeft < 40 && villainLeft > 20 && heroTop >= 130) {
-        villain.style.animation = "none";
-        alert("Game over, press spacebar to start");
-    }
+  if (villainLeft < 40 && villainLeft > 20 && heroTop >= 130) {
+    villain.style.animation = "none";
+    alert("Game over, press spacebar to start");
+  }
 }, 10);
+
