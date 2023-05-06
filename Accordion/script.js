9ef3f154387f6ce5d53862b78f722d16ff76145d
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-06 15:50:45 +0300

    Updated script.js
    Using querySelectorAll instead of getElementsByClassName to get the active accordions.

--- Accordion/script.js
+++ Accordion/script.js
@@ -1,30 +1,25 @@
-const acc = document.querySelectorAll(".accordion");
+const accordions = document.querySelectorAll(".accordion");
 
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    // this.classList.toggle("active");
-    const desc = this.nextElementSibling;
-    const allDesc = document.querySelectorAll(".desc");
-    const activeAcc = document.getElementsByClassName("accordion active");
-    // console.log(desc);
-    // console.log(desc.scrollHeight);
-    if (desc.style.maxHeight) {
-      desc.style.maxHeight = null;
-      this.classList.remove("active");
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    const description = accordion.nextElementSibling;
+    const allDescriptions = document.querySelectorAll(".desc");
+    const activeAccordions = document.querySelectorAll(".accordion.active");
+
+    if (description.style.maxHeight) {
+      description.style.maxHeight = null;
+      accordion.classList.remove("active");
     } else {
-      // Remove active class from all tabs
-      for (let i = 0; i < activeAcc.length; i++) {
-        activeAcc[i].classList.remove("active");
-      }
+      activeAccordions.forEach((activeAccordion) => {
+        activeAccordion.classList.remove("active");
+      });
 
-      // Set the maxHeight of all desc to 0
-      for (let i = 0; i < allDesc.length; i++) {
-        allDesc[i].style.maxHeight = null;
-      }
+      allDescriptions.forEach((desc) => {
+        desc.style.maxHeight = null;
+      });
 
-      // open the current tab clicked
-      desc.style.maxHeight = desc.scrollHeight + "px";
-      this.classList.add("active");
+      description.style.maxHeight = description.scrollHeight + "px";
+      accordion.classList.add("active");
     }
   });
-}
+});
