fa9d2a87fa3147d1a51104ece398c1115c3707a4
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-08 15:47:34 +0300

    Updated script.js
    Renamed the joke.innerHTML assignment to displayJoke to make it clear what the function does.

--- Async Await - Random Joke Gen/script.js
+++ Async Await - Random Joke Gen/script.js
@@ -1,29 +1,19 @@
-// http://api.icndb.com/jokes/random
-
 const joke = document.querySelector(".joke");
 const btn = document.querySelector(".btn");
 const url = "http://api.icndb.com/jokes/random";
 
 btn.addEventListener("click", getJoke);
 
-// function getJoke() {
-//   fetch(url)
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       joke.innerHTML = data.value.joke;
-//     });
-// }
+function displayJoke(jokeText) {
+  joke.innerHTML = jokeText;
+}
 
 async function getJoke() {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    joke.innerHTML = data.value.joke;
-    //   console.log(data);
+    displayJoke(data.value.joke);
   } catch (error) {
-    joke.innerHTML = error.message;
+    displayJoke(error.message);
   }
 }
