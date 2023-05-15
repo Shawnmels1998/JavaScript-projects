fd3502155cf2bd45edb34d4f7f303333a23b706b
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-15 16:41:33 +0300

    Updated script.js

--- Multi Step Form/script.js
+++ Multi Step Form/script.js
@@ -1,52 +1,66 @@
-const formItems = document.getElementsByClassName("form-item");
-const button = document.getElementsByClassName("btn");
-const steps = document.getElementsByClassName("step");
+const formItems = document.querySelectorAll(".form-item");
+const buttonPrev = document.querySelector(".btn-prev");
+const buttonNext = document.querySelector(".btn-next");
+const steps = document.querySelectorAll(".step");
 
 let currentFormItem = 0;
-formItems[currentFormItem].style.display = "block";
+showFormStep(currentFormItem);
 
-if (currentFormItem == 0) {
-  button[0].style.display = "none";
-  steps[0].style.backgroundColor = "#1e9df7";
+buttonPrev.style.display = "none";
+steps[currentFormItem].style.backgroundColor = "#1e9df7";
+
+function showFormStep(stepIndex) {
+  formItems.forEach((item, index) => {
+    if (index === stepIndex) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
-// Next Button
-button[1].addEventListener("click", () => {
-  currentFormItem++;
-  const prevFormItem = currentFormItem - 1;
-
-  if (currentFormItem > 0 && currentFormItem < 4) {
-    button[0].style.display = "inline-block";
-    formItems[currentFormItem].style.display = "block";
-    formItems[prevFormItem].style.display = "none";
-    steps[currentFormItem].style.backgroundColor = "#1e9df7";
-
-    if (currentFormItem == 3) {
-      button[1].innerHTML = "Submit";
-    }
+function updateButtonLabels() {
+  if (currentFormItem === 0) {
+    buttonPrev.style.display = "none";
   } else {
-    if (currentFormItem > 3) {
-      // Validate the form
-      alert("Form Sunmitted Successfully");
+    buttonPrev.style.display = "inline-block";
+  }
+  if (currentFormItem === formItems.length - 1) {
+    buttonNext.innerHTML = "Submit";
+  } else {
+    buttonNext.innerHTML = "Next";
+  }
+}
+
+function updateStepIndicators() {
+  steps.forEach((step, index) => {
+    if (index < currentFormItem) {
+      step.style.backgroundColor = "#cfd2d4";
+    } else if (index === currentFormItem) {
+      step.style.backgroundColor = "#1e9df7";
+    } else {
+      step.style.backgroundColor = "";
     }
+  });
+}
+
+buttonNext.addEventListener("click", () => {
+  if (currentFormItem < formItems.length - 1) {
+    currentFormItem++;
+    showFormStep(currentFormItem);
+    updateButtonLabels();
+    updateStepIndicators();
+  } else {
+    // Validate the form
+    alert("Form submitted successfully");
   }
 });
 
-// Prev Button
-button[0].addEventListener("click", () => {
+buttonPrev.addEventListener("click", () => {
   if (currentFormItem > 0) {
     currentFormItem--;
-    const nextFormItem = currentFormItem + 1;
-    formItems[currentFormItem].style.display = "block";
-    formItems[nextFormItem].style.display = "none";
-    steps[nextFormItem].style.backgroundColor = "#cfd2d4";
-
-    if (currentFormItem == 0) {
-      button[0].style.display = "none";
-    }
-
-    if (currentFormItem < 3) {
-      button[1].innerHTML = "Next";
-    }
+    showFormStep(currentFormItem);
+    updateButtonLabels();
+    updateStepIndicators();
   }
 });
