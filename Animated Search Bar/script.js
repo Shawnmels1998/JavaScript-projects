551813019d580e06f8fdeda51ce54bbf84eacb5d
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-17 16:01:30 +0300

    Updated script.js

--- Animated Search Bar/script.js
+++ Animated Search Bar/script.js
@@ -3,34 +3,33 @@ const closeIcon = document.querySelector(".fa-times");
 const searchEl = document.querySelector(".search");
 const searchInput = document.querySelector(".search input");
 
-searchIcon.addEventListener("click", () => {
-  searchIcon.style.transform = "translateY(200%)";
-  closeIcon.style.transform = "translateY(0)";
-  showSearchEl();
-});
-
-closeIcon.addEventListener("click", () => {
-  searchIcon.style.transform = "translateY(0)";
-  closeIcon.style.transform = "translateY(-200%)";
-  hideSearchEl();
-});
-
-function showSearchEl() {
+const showSearchEl = () => {
   searchEl.style.transform = "translateX(0)";
   setTimeout(() => {
     searchEl.style.width = "30rem";
-  }, 1000);
-  setTimeout(() => {
     searchInput.setAttribute("placeholder", "Search...");
-  }, 2000);
-}
+  }, 1000);
+};
 
-function hideSearchEl() {
+const hideSearchEl = () => {
   searchInput.setAttribute("placeholder", "");
   setTimeout(() => {
     searchEl.style.width = "4.5rem";
-  }, 1000);
-  setTimeout(() => {
     searchEl.style.transform = "translateX(200%)";
-  }, 2000);
-}
+  }, 1000);
+};
+
+const handleClickSearchIcon = () => {
+  searchIcon.style.transform = "translateY(200%)";
+  closeIcon.style.transform = "translateY(0)";
+  showSearchEl();
+};
+
+const handleClickCloseIcon = () => {
+  searchIcon.style.transform = "translateY(0)";
+  closeIcon.style.transform = "translateY(-200%)";
+  hideSearchEl();
+};
+
+searchIcon.addEventListener("click", handleClickSearchIcon);
+closeIcon.addEventListener("click", handleClickCloseIcon);
