cf92b5ee682afb23c5c5743a71839aa68912a3f3
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-20 14:54:21 +0300

    Updated script.js
    introduced a getElement function to reduce code duplication for obtaining DOM elements.
    Used the getElement function to simplify the code for obtaining DOM elements.
    Changed the closeModal method to a standalone function to avoid the issue with this context.
    Removed the unnecessary this.closeModal reference in the trigger method.
    Moved the changeBg function declaration before its usage.
    Renamed the changeBag variable to showConfirm for clarity.
    Made changeBg a standalone arrow function for better consistency and readability.

--- Custom Confirm Box/script.js
+++ Custom Confirm Box/script.js
@@ -1,13 +1,14 @@
-const btn1 = document.querySelector(".btn-1");
-const btn2 = document.querySelector(".btn-2");
-const confirmEl = document.querySelector(".confirm");
-const closeEl = document.querySelector(".close");
-const title = document.querySelector(".title");
-const content = document.querySelector(".content");
-const btnOk = document.querySelector(".btn-ok");
-const btnCancel = document.querySelector(".btn-cancel");
+const getElement = (selector) => document.querySelector(selector);
 
-// Custom Confirm Box
+const btn1 = getElement(".btn-1");
+const btn2 = getElement(".btn-2");
+const confirmEl = getElement(".confirm");
+const closeEl = getElement(".close");
+const title = getElement(".title");
+const content = getElement(".content");
+const btnOk = getElement(".btn-ok");
+const btnCancel = getElement(".btn-cancel");
+
 class ShowConfirm {
   constructor(title, content, ok, cancel) {
     this.title = title;
@@ -24,53 +25,39 @@ class ShowConfirm {
 
     confirmEl.classList.remove("close-modal");
 
-    closeEl.addEventListener("click", this.closeModal);
-    btnCancel.addEventListener("click", this.closeModal);
+    const closeModal = () => {
+      confirmEl.classList.add("close-modal");
+    };
+
+    closeEl.addEventListener("click", closeModal);
+    btnCancel.addEventListener("click", closeModal);
 
     btnOk.addEventListener("click", () => {
       callbackFn();
-      this.closeModal();
+      closeModal();
     });
-  }
-
-  closeModal() {
-    confirmEl.classList.add("close-modal");
   }
 }
 
-// Btn Event Listeners
+const changeBg = (color) => {
+  const setBg = () => {
+    document.body.style.backgroundColor = color;
+  };
+
+  const showConfirm = new ShowConfirm(
+    "Change Background",
+    "You are about to change the background!",
+    "Change",
+    "Cancel"
+  );
+
+  showConfirm.trigger(setBg);
+};
+
 btn1.addEventListener("click", () => {
   changeBg("red");
 });
+
 btn2.addEventListener("click", () => {
   changeBg("purple");
 });
-
-const changeBag = new ShowConfirm(
-  "Change Background",
-  "You are about to change the background!",
-  "Change",
-  "Cancel"
-);
-
-function changeBg(color) {
-  changeBag.trigger(setBg);
-  function setBg() {
-    document.body.style.backgroundColor = color;
-  }
-}
-
-// Defaul Confirm Example
-// btn1.addEventListener("click", () => {
-//   changeBg("red");
-// });
-// btn2.addEventListener("click", () => {
-//   changeBg("purple");
-// });
-
-// function changeBg(color) {
-//   let x = confirm("Change Backround to " + color);
-//   if (x) {
-//     document.body.style.backgroundColor = color;
-//   }
-// }
