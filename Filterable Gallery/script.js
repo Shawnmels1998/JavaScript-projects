e0fe6436dba6bae9d82243b16df94e8843d75ec2
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-22 18:45:26 +0300

    Updated script.js

--- Filterable Gallery/script.js
+++ Filterable Gallery/script.js
@@ -2,17 +2,15 @@ const galleryFilter = document.querySelector(".gallery-filter");
 const galleryImages = document.querySelectorAll(".image");
 
 galleryFilter.addEventListener("click", (e) => {
-  if (e.target.classList.contains("filter-gallery")) {
-    galleryFilter.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
+const target = e.target;
+if (target.classList.contains("filter-gallery")) {
+const activeFilter = galleryFilter.querySelector(".active");
+activeFilter.classList.remove("active");
+target.classList.add("active");
 
-    const filter = e.target.dataset.filter;
-    galleryImages.forEach((image) => {
-      if (filter === "all" || image.classList.contains(filter)) {
-        image.style.display = "block";
-      } else {
-        image.style.display = "none";
-      }
-    });
-  }
+const filter = target.dataset.filter;
+galleryImages.forEach((image) => {
+  image.style.display = (filter === "all" || image.classList.contains(filter)) ? "block" : "none";
+});
+}
 });
