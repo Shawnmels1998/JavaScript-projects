76b426eb2b3318c1bae358ded6dec5c00e59ca1f
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-23 20:28:11 +0300

    Updated script.js
    Removed the unnecessary assignment of speech.voice = speechSynthesis.speak(speech), as it should only be speechSynthesis.speak(speech) without assigning the return value to speech.voice.

--- Speech Synthesis/script.js
+++ Speech Synthesis/script.js
@@ -1,28 +1,24 @@
 const searchForm = document.querySelector("#search-form");
 const input = document.querySelector("#search-input");
-
 const speechBtnDiv = document.querySelector("#speech-btn");
 const micBtn = document.querySelector(".btn .fas");
 const instruction = document.querySelector(".instruction");
-
 const speechSynthesis = window.speechSynthesis;
 
 // Check for browser support
 if (speechSynthesis) {
   console.log("Speech Synthesis Supported");
-
   micBtn.addEventListener("click", speak);
 
   function speak(e) {
     e.preventDefault();
     const inputValue = input.value;
-    const speech = new SpeechSynthesisUtterance();
+    const speech = new SpeechSynthesisUtterance(inputValue);
     speech.lang = "en-US";
-    speech.text = inputValue;
     speech.volume = "1";
     speech.rate = "1";
     speech.pitch = "1";
-    speech.voice = speechSynthesis.speak(speech);
+    speechSynthesis.speak(speech);
   }
 } else {
   console.log("Speech Synthesis Not Supported");
