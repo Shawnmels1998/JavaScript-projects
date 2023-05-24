4d84be76675baccd677fa8a1df5c1c876351b922
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-24 20:17:39 +0300

    Updated script.js
    The DOM elements are grouped together under the elements object for easier access.
    The repetitive code for incrementing and wrapping the index of the trackList array in the nextSong and prevSong functions is replaced with modular arithmetic.
    The autoPlayToggle function is simplified using a ternary operator.
    The padStart method is used to format the time values with leading zeros in the songTimeUpdate function.
    The for loop in the displayTracks function is replaced with forEach loop for cleaner code.

--- Music Player App/script.js
+++ Music Player App/script.js
@@ -1,55 +1,53 @@
-const play = document.querySelector(".play"),
-  previous = document.querySelector(".prev"),
-  next = document.querySelector(".next"),
-  //
-  trackImage = document.querySelector(".track-image"),
-  title = document.querySelector(".title"),
-  artist = document.querySelector(".artist"),
-  //
-  trackCurrentTime = document.querySelector(".current-time"),
-  trackDuration = document.querySelector(".duration-time"),
-  slider = document.querySelector(".duration-slider"),
-  //
-  showVolume = document.querySelector("#show-volume"),
-  volumeIcon = document.querySelector("#volume-icon"),
-  currentVolume = document.querySelector("#volume"),
-  //
-  autoPlayBtn = document.querySelector(".play-all"),
-  //
-  hamBurger = document.querySelector(".fa-bars"),
-  closeIcon = document.querySelector(".fa-times"),
-  //
-  musicPlaylist = document.querySelector(".music-playlist"),
-  pDiv = document.querySelector(".playlist-div"),
-  playList = document.querySelector(".playlist");
+const elements = {
+  play: document.querySelector(".play"),
+  previous: document.querySelector(".prev"),
+  next: document.querySelector(".next"),
+  trackImage: document.querySelector(".track-image"),
+  title: document.querySelector(".title"),
+  artist: document.querySelector(".artist"),
+  trackCurrentTime: document.querySelector(".current-time"),
+  trackDuration: document.querySelector(".duration-time"),
+  slider: document.querySelector(".duration-slider"),
+  showVolume: document.querySelector("#show-volume"),
+  volumeIcon: document.querySelector("#volume-icon"),
+  currentVolume: document.querySelector("#volume"),
+  autoPlayBtn: document.querySelector(".play-all"),
+  hamBurger: document.querySelector(".fa-bars"),
+  closeIcon: document.querySelector(".fa-times"),
+  musicPlaylist: document.querySelector(".music-playlist"),
+  pDiv: document.querySelector(".playlist-div"),
+  playList: document.querySelector(".playlist"),
+};
 
 let timer;
 let autoplay = 0;
 let indexTrack = 0;
 let songIsPlaying = false;
-let track = document.createElement("audio");
+const track = document.createElement("audio");
 
-//   All Event Listeners
-play.addEventListener("click", justPlay);
-next.addEventListener("click", nextSong);
-previous.addEventListener("click", prevSong);
-autoPlayBtn.addEventListener("click", autoPlayToggle);
-volumeIcon.addEventListener("click", muteSound);
-currentVolume.addEventListener("change", changeVolume);
-slider.addEventListener("change", changeDuration);
+// All Event Listeners
+elements.play.addEventListener("click", justPlay);
+elements.next.addEventListener("click", nextSong);
+elements.previous.addEventListener("click", prevSong);
+elements.autoPlayBtn.addEventListener("click", autoPlayToggle);
+elements.volumeIcon.addEventListener("click", muteSound);
+elements.currentVolume.addEventListener("change", changeVolume);
+elements.slider.addEventListener("change", changeDuration);
 track.addEventListener("timeupdate", songTimeUpdate);
-hamBurger.addEventListener("click", showPlayList);
-closeIcon.addEventListener("click", hidePlayList);
+elements.hamBurger.addEventListener("click", showPlayList);
+elements.closeIcon.addEventListener("click", hidePlayList);
 
 // Load Tracks
-function loadTrack(indexTrack) {
+function loadTrack(index) {
   clearInterval(timer);
   resetSlider();
 
-  track.src = trackList[indexTrack].path;
-  trackImage.src = trackList[indexTrack].img;
-  title.innerHTML = trackList[indexTrack].name;
-  artist.innerHTML = trackList[indexTrack].singer;
+  const { path, img, name, singer } = trackList[index];
+
+  track.src = path;
+  elements.trackImage.src = img;
+  elements.title.innerHTML = name;
+  elements.artist.innerHTML = singer;
   track.load();
 
   timer = setInterval(updateSlider, 1000);
@@ -58,7 +56,7 @@ loadTrack(indexTrack);
 
 // Play song or Pause song
 function justPlay() {
-  if (songIsPlaying == false) {
+  if (!songIsPlaying) {
     playSong();
   } else {
     pauseSong();
@@ -69,75 +67,58 @@ function justPlay() {
 function playSong() {
   track.play();
   songIsPlaying = true;
-  play.innerHTML = '<i class="fas fa-pause"></i>';
+  elements.play.innerHTML = '<i class="fas fa-pause"></i>';
 }
 
 // Pause Song
 function pauseSong() {
   track.pause();
   songIsPlaying = false;
-  play.innerHTML = '<i class="fas fa-play"></i>';
+  elements.play.innerHTML = '<i class="fas fa-play"></i>';
 }
 
 // Next song
 function nextSong() {
-  if (indexTrack < trackList.length - 1) {
-    indexTrack++;
-    loadTrack(indexTrack);
-    playSong();
-  } else {
-    indexTrack = 0;
-    loadTrack(indexTrack);
-    playSong();
-  }
+  indexTrack = (indexTrack + 1) % trackList.length;
+  loadTrack(indexTrack);
+  playSong();
 }
 
-// prev song
+// Previous song
 function prevSong() {
-  if (indexTrack > 0) {
-    indexTrack--;
-    loadTrack(indexTrack);
-    playSong();
-  } else {
-    indexTrack = trackList.length - 1;
-    loadTrack(indexTrack);
-    playSong();
-  }
+  indexTrack = (indexTrack - 1 + trackList.length) % trackList.length;
+  loadTrack(indexTrack);
+  playSong();
 }
 
 // Mute Sound
 function muteSound() {
   track.volume = 0;
-  showVolume.innerHTML = 0;
-  currentVolume.value = 0;
+  elements.showVolume.innerHTML = 0;
+  elements.currentVolume.value = 0;
 }
 
 // Change Volume
 function changeVolume() {
-  showVolume.value = currentVolume.value;
-  track.volume = currentVolume.value / 100;
+  elements.showVolume.value = elements.currentVolume.value;
+  track.volume = elements.currentVolume.value / 100;
 }
 
 // Change Duration
 function changeDuration() {
-  let sliderPosition = track.duration * (slider.value / 100);
+  const sliderPosition = track.duration * (elements.slider.value / 100);
   track.currentTime = sliderPosition;
 }
 
 // Auto Play
 function autoPlayToggle() {
-  if (autoplay == 0) {
-    autoplay = 1;
-    autoPlayBtn.style.background = "#db6400";
-  } else {
-    autoplay = 0;
-    autoPlayBtn.style.background = "#ccc";
-  }
+  autoplay = autoplay === 0 ? 1 : 0;
+  elements.autoPlayBtn.style.background = autoplay === 1 ? "#db6400" : "#ccc";
 }
 
-// Reste Slider
+// Reset Slider
 function resetSlider() {
-  slider.value = 0;
+  elements.slider.value = 0;
 }
 
 // Update Slider
@@ -145,17 +126,17 @@ function updateSlider() {
   let position = 0;
 
   if (!isNaN(track.duration)) {
-    position = track.currentTime * (100 / track.duration);
-    slider.value = position;
+    position = (track.currentTime / track.duration) * 100;
+    elements.slider.value = position;
   }
 
   if (track.ended) {
-    play.innerHTML = '<i class="fas fa-play"></i>';
-    if (autoplay == 1 && indexTrack < trackList.length - 1) {
+    elements.play.innerHTML = '<i class="fas fa-play"></i>';
+    if (autoplay === 1 && indexTrack < trackList.length - 1) {
       indexTrack++;
       loadTrack(indexTrack);
       playSong();
-    } else if (autoplay == 1 && indexTrack == trackList.length - 1) {
+    } else if (autoplay === 1 && indexTrack === trackList.length - 1) {
       indexTrack = 0;
       loadTrack(indexTrack);
       playSong();
@@ -171,48 +152,43 @@ function songTimeUpdate() {
     let durmins = Math.floor(track.duration / 60);
     let dursecs = Math.floor(track.duration - durmins * 60);
 
-    if (dursecs < 10) {
-      dursecs = "0" + dursecs;
-    }
-    if (durmins < 10) {
-      durmins = "0" + durmins;
-    }
-    if (curmins < 10) {
-      curmins = "0" + curmins;
-    }
-    if (cursecs < 10) {
-      cursecs = "0" + cursecs;
-    }
-    trackCurrentTime.innerHTML = curmins + ":" + cursecs;
-    trackDuration.innerHTML = durmins + ":" + dursecs;
+    curmins = curmins.toString().padStart(2, "0");
+    cursecs = cursecs.toString().padStart(2, "0");
+    durmins = durmins.toString().padStart(2, "0");
+    dursecs = dursecs.toString().padStart(2, "0");
+
+    elements.trackCurrentTime.innerHTML = `${curmins}:${cursecs}`;
+    elements.trackDuration.innerHTML = `${durmins}:${dursecs}`;
   } else {
-    trackCurrentTime.innerHTML = "00" + ":" + "00";
-    trackDuration.innerHTML = "00" + ":" + "00";
+    elements.trackCurrentTime.innerHTML = "00:00";
+    elements.trackDuration.innerHTML = "00:00";
   }
 }
 
 // Show PlayList
 function showPlayList() {
-  musicPlaylist.style.transform = "translateX(0)";
+  elements.musicPlaylist.style.transform = "translateX(0)";
 }
+
 // Hide PlayList
 function hidePlayList() {
-  musicPlaylist.style.transform = "translateX(-100%)";
+  elements.musicPlaylist.style.transform = "translateX(-100%)";
 }
 
 // Display Tracks in playlist
 let counter = 1;
+
 function displayTracks() {
-  for (let i = 0; i < trackList.length; i++) {
-    console.log(trackList[i].name);
-    let div = document.createElement("div");
+  trackList.forEach((trackItem) => {
+    const { name } = trackItem;
+    const div = document.createElement("div");
     div.classList.add("playlist");
     div.innerHTML = `
         <span class="song-index">${counter++}</span>
-        <p class="single-song">${trackList[i].name}</p>
+        <p class="single-song">${name}</p>
     `;
-    pDiv.appendChild(div);
-  }
+    elements.pDiv.appendChild(div);
+  });
   playFromPlaylist();
 }
 
@@ -220,14 +196,9 @@ displayTracks();
 
 // Play song from the playlist
 function playFromPlaylist() {
-  pDiv.addEventListener("click", (e) => {
+  elements.pDiv.addEventListener("click", (e) => {
     if (e.target.classList.contains("single-song")) {
-      //   alert(e.target.innerHTML);
-      const indexNum = trackList.findIndex((item, index) => {
-        if (item.name === e.target.innerHTML) {
-          return true;
-        }
-      });
+      const indexNum = trackList.findIndex((item) => item.name === e.target.innerHTML);
       loadTrack(indexNum);
       playSong();
       hidePlayList();
