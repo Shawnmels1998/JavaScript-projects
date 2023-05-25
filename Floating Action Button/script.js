0578be158c482ed13b0f04325b71764155f8f7a2
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-25 16:42:48 +0300

    Updated script.js

--- Floating Action Button/script.js
+++ Floating Action Button/script.js
@@ -1,6 +1,8 @@
 const fab = document.getElementById("fab");
 const fabBtns = document.querySelector(".fab-btns");
 
-fab.addEventListener("click", () => {
+const toggleFabBtns = () => {
   fabBtns.classList.toggle("show");
-});
+};
+
+fab.addEventListener("click", toggleFabBtns);
