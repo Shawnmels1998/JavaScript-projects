b8b796a725091a5d9946594b6130e39bc65e16cf
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-29 20:34:56 +0300

    Updated script.js

--- Pricing Table/script.js
+++ Pricing Table/script.js
@@ -1,21 +1,21 @@
 const checkBox = document.querySelector("#checkbox");
 const slider = document.querySelector(".slider");
-const basic = document.querySelector(".basic");
-const prof = document.querySelector(".prof");
-const master = document.querySelector(".master");
+const levels = {
+  basic: document.querySelector(".basic"),
+  prof: document.querySelector(".prof"),
+  master: document.querySelector(".master"),
+};
 
 slider.addEventListener("click", () => {
   if (!checkBox.checked) {
-    [basic.textContent, prof.textContent, master.textContent] = [
-      "120",
-      "240",
-      "360",
-    ];
+    setLevelPrices("120", "240", "360");
   } else {
-    [basic.textContent, prof.textContent, master.textContent] = [
-      "9.99",
-      "19.99",
-      "29.99",
-    ];
+    setLevelPrices("9.99", "19.99", "29.99");
   }
 });
+
+function setLevelPrices(basicPrice, profPrice, masterPrice) {
+  levels.basic.textContent = basicPrice;
+  levels.prof.textContent = profPrice;
+  levels.master.textContent = masterPrice;
+}
