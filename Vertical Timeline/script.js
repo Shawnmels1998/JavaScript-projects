c096451a1e2399b8a3e907454735910d483bb19e
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-05-31 16:20:42 +0300

    Updated script.js

--- Vertical Timeline/script.js
+++ Vertical Timeline/script.js
@@ -1,28 +1,28 @@
-(function () {
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function slideIn() {
   const items = document.querySelectorAll(".timeline li");
-
-  function isElementInViewport(el) {
-    let rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function slideIn() {
-    for (let i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("slide-in");
-      } else {
-        items[i].classList.remove("slide-in");
-      }
+  items.forEach((item) => {
+    if (isElementInViewport(item)) {
+      item.classList.add("slide-in");
+    } else {
+      item.classList.remove("slide-in");
     }
-  }
+  });
+}
 
+function initialize() {
   window.addEventListener("load", slideIn);
   window.addEventListener("scroll", slideIn);
   window.addEventListener("resize", slideIn);
-})();
+}
+
+initialize();
