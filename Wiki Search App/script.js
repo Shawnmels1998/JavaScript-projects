533c62c59118035ea77011b8e6011700abef9d1e
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-06-02 15:02:19 +0300

    Updated script.js

--- Wiki Search App/script.js
+++ Wiki Search App/script.js
@@ -7,60 +7,48 @@ const line = document.querySelector("hr");
 const apiURL =
   "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=";
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(e) {
+  e.preventDefault();
   const searchValue = search.value;
   if (searchValue === "") {
-    errorMessage("Search cannot be empty, please enter a search term.");
+    displayErrorMessage("Search cannot be empty, please enter a search term.");
   } else {
     getResult(searchValue);
   }
-});
+}
 
-// Error Message
-function errorMessage(msg) {
+function displayErrorMessage(msg) {
   errorMsg.style.display = "block";
   line.style.display = "block";
   errorMsg.textContent = msg;
 }
 
-async function getResult(searchVal) {
-  const response = await fetch(apiURL + searchVal);
-  const results = await response.json();
+async function getResult(searchValue) {
+  const response = await fetch(apiURL + searchValue);
+  const { query } = await response.json();
 
-  console.log(results);
-  if (results.query.search.length == 0) {
-    return errorMessage("Invalid search, please enter another search term.");
+  if (query.search.length === 0) {
+    return displayErrorMessage("Invalid search, please enter another search term.");
   } else {
-    displayResults(results);
+    displayResults(query.search);
   }
 }
 
-// Display Results
 function displayResults(results) {
   line.style.display = "block";
   let output = "";
-  results.query.search.forEach((result) => {
-    let resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
+  results.forEach((result) => {
+    const resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
     output += `
-    <div class="result p-2">
-    <a href="${resultURL}" target="_blank" rel="noopener" class="h3 fw-bold"
-      >${result.title}</a
-    >
-    <br />
-    <a
-      href="${resultURL}"
-      target="_blank"
-      rel="noopener"
-      class="fs-5 text-success"
-      >${resultURL}</a
-    >
-    <p class="fs-5">
-    ${result.snippet}
-    </p>
-  </div>
+      <div class="result p-2">
+        <a href="${resultURL}" target="_blank" rel="noopener" class="h3 fw-bold">${result.title}</a>
+        <br />
+        <a href="${resultURL}" target="_blank" rel="noopener" class="fs-5 text-success">${resultURL}</a>
+        <p class="fs-5">${result.snippet}</p>
+      </div>
     `;
-    searchResults.innerHTML = output;
   });
+  searchResults.innerHTML = output;
 }
