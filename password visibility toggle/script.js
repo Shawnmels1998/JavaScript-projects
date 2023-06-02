908ab18618d05a30ac4d2ef01f54e5168268581b
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-06-02 14:59:49 +0300

    Updated script.js

--- password visibility toggle/script.js
+++ password visibility toggle/script.js
@@ -1,12 +1,23 @@
 const passwordInput = document.querySelector("#password");
 const togglePasswordBtn = document.querySelector("#eye");
 
+togglePasswordBtn.addEventListener("click", togglePasswordVisibility);
+
 function togglePasswordVisibility() {
   const isPasswordVisible = passwordInput.type === "text";
-  passwordInput.type = isPasswordVisible ? "password" : "text";
-  const eyeIconClassToReplace = isPasswordVisible ? "fa-eye-slash" : "fa-eye";
-  const eyeIconClassToReplaceWith = isPasswordVisible ? "fa-eye" : "fa-eye-slash";
-  togglePasswordBtn.classList.replace(eyeIconClassToReplace, eyeIconClassToReplaceWith);
+  const newPasswordType = isPasswordVisible ? "password" : "text";
+  const eyeIconClassToRemove = isPasswordVisible ? "fa-eye-slash" : "fa-eye";
+  const eyeIconClassToAdd = isPasswordVisible ? "fa-eye" : "fa-eye-slash";
+  
+  updatePasswordVisibility(newPasswordType);
+  updateEyeIconClass(eyeIconClassToRemove, eyeIconClassToAdd);
 }
 
-togglePasswordBtn.addEventListener("click", togglePasswordVisibility);
+function updatePasswordVisibility(newType) {
+  passwordInput.type = newType;
+}
+
+function updateEyeIconClass(classToRemove, classToAdd) {
+  togglePasswordBtn.classList.remove(classToRemove);
+  togglePasswordBtn.classList.add(classToAdd);
+}
