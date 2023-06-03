e581bbe01f39789b2f0dfa7afeae459b20aea540
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-06-03 20:26:20 +0300

    Updated script.js

--- Word Count Tool/script.js
+++ Word Count Tool/script.js
@@ -1,49 +1,47 @@
-let input = document.querySelector(".input"),
-  character = document.querySelector(".character"),
-  word = document.querySelector(".word"),
-  readingTime = document.querySelector(".reading-time"),
-  wordLimit = document.querySelector(".word-limit"),
-  WORD_LIMIT = 100;
+const input = document.querySelector(".input");
+const character = document.querySelector(".character");
+const word = document.querySelector(".word");
+const readingTime = document.querySelector(".reading-time");
+const wordLimit = document.querySelector(".word-limit");
+const WORD_LIMIT = 100;
 
-input.addEventListener("keyup", characterCount);
-input.addEventListener("keyup", wordCounter);
+input.addEventListener("keyup", handleInput);
 
-function characterCount() {
-  character.innerHTML = input.value.length;
-}
-
-function wordCounter(e) {
-  let words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+function handleInput() {
+  const value = input.value;
+  character.innerHTML = value.length;
+  const words = value.match(/\b[-?(\w+)?]+\b/gi);
 
   if (words) {
-    word.innerHTML = words.length;
-    wordLimit.innerHTML = WORD_LIMIT - words.length;
+    const wordCount = words.length;
+    word.innerHTML = wordCount;
+    wordLimit.innerHTML = WORD_LIMIT - wordCount;
+    handleWordLimit();
+    handleReadingTime(wordCount);
   } else {
     word.innerHTML = 0;
+    wordLimit.innerHTML = WORD_LIMIT;
+    readingTime.innerHTML = "0s";
   }
+}
 
-  //   Word Limit Block of Code
+function handleWordLimit() {
   input.addEventListener("keydown", function (e) {
-    words = input.value.match(/\b[-?(\w+)?]+\b/gi);
-    if (words) {
-      if (words.length > WORD_LIMIT - 1 && e.code !== "Backspace") {
-        e.preventDefault();
-        // console.log("Word limit reached");
-      }
+    const value = input.value;
+    const words = value.match(/\b[-?(\w+)?]+\b/gi);
+    if (words && words.length > WORD_LIMIT - 1 && e.code !== "Backspace") {
+      e.preventDefault();
     }
   });
+}
 
-  //   Reading time based on 225 words/min
-  if (words) {
-    let seconds = Math.floor((words.length * 60) / 225);
-    if (seconds > 59) {
-      let minutes = Math.floor(seconds / 60);
-      seconds = seconds - minutes * 60;
-      readingTime.innerHTML = minutes + "m" + seconds + "s";
-    } else {
-      readingTime.innerHTML = seconds + "s";
-    }
+function handleReadingTime(wordCount) {
+  const seconds = Math.floor((wordCount * 60) / 225);
+  if (seconds > 59) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds - minutes * 60;
+    readingTime.innerHTML = `${minutes}m${remainingSeconds}s`;
   } else {
-    readingTime.innerHTML = "0s";
+    readingTime.innerHTML = `${seconds}s`;
   }
 }
