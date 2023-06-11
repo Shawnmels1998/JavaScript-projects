e48dcac4c8bcf830ff3e9daba4477de29dcb4dcf
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-06-11 15:23:37 +0300

    Updated script.js

--- Random hex-color generator/script.js
+++ Random hex-color generator/script.js
@@ -1,11 +1,17 @@
-const hex = document.querySelector(".hex");
-const btn = document.querySelector(".generate");
+const elements = {
+  hex: document.querySelector(".hex"),
+  btn: document.querySelector(".generate"),
+};
 
-btn.addEventListener("click", generateColor);
+elements.btn.addEventListener("click", generateColor);
 
 function generateColor() {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    // console.log(randomColor);
-    document.body.style.backgroundColor = "#" + randomColor;
-    hex.innerHTML = "#" + randomColor;
+  const randomColor = getRandomColor();
+  document.body.style.backgroundColor = randomColor;
+  elements.hex.innerHTML = randomColor;
+}
+
+function getRandomColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + randomColor;
 }
