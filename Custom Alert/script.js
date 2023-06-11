c60b36ea0538e1acd3991f4bda40748434d272a5
Shawn Mels <84438879+Shawnmels1998@users.noreply.github.com>
2023-06-11 15:21:35 +0300

    Updated script.js

--- Custom Alert/script.js
+++ Custom Alert/script.js
@@ -1,25 +1,31 @@
-const main = document.querySelector(".main");
-const alertBox = document.querySelector(".alert");
-const exclamationIcon = document.querySelector(".fa-exclamation-circle");
-const msg = document.querySelector(".msg");
-const closeBtn = document.querySelector(".close-btn");
-const closeIcon = document.querySelector(".fa-times");
+const elements = {
+  main: document.querySelector(".main"),
+  alertBox: document.querySelector(".alert"),
+  exclamationIcon: document.querySelector(".fa-exclamation-circle"),
+  msg: document.querySelector(".msg"),
+  closeBtn: document.querySelector(".close-btn"),
+  closeIcon: document.querySelector(".fa-times"),
+};
 
-// ShowAlert Class
 class ShowAlert {
   constructor(state, borderColor, color) {
+    this.alertBox = elements.alertBox;
+    this.msg = elements.msg;
+    this.closeBtn = elements.closeBtn;
     this.state = state;
     this.borderColor = borderColor;
     this.color = color;
   }
 
   trigger(message) {
+    const { alertBox, msg, closeBtn } = this;
+
     alertBox.style.background = this.state;
     alertBox.style.borderColor = this.borderColor;
     msg.textContent = message;
     msg.style.color = this.color;
-    exclamationIcon.style.color = this.color;
-    closeIcon.style.color = this.color;
+    elements.exclamationIcon.style.color = this.color;
+    elements.closeIcon.style.color = this.color;
 
     alertBox.classList.add("show");
     alertBox.classList.remove("hide");
@@ -37,7 +43,7 @@ class ShowAlert {
 const warning = new ShowAlert("#ffdb9b", "#ffa502", "#ce8500");
 const danger = new ShowAlert("#f8d7da", "#d1281f", "#721c24");
 
-main.addEventListener("click", (e) => {
+elements.main.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn-danger")) {
     danger.trigger("Alert: This is a dangerous alert!");
   } else if (e.target.classList.contains("btn-warning")) {
